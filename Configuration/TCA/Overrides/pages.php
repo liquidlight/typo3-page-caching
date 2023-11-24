cd5ab16d9e435b12a64b6fe4b8e283e56c9b69ab
Mike Street <mikestreety@gmail.com>
2023-11-24 20:18:26 +0000

    feat: Allow comaptability with TYPO3 v9

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3') or die();
+defined('TYPO3_MODE') or die();
 
 $GLOBALS['TCA']['pages']['columns']['tx_staticfilecache_cache']['description'] = 'LLL:EXT:caching/Resources/Private/Language/locallang_tca.xlf:tx_staticfilecache_cache.description';
 $GLOBALS['TCA']['pages']['columns']['tx_staticfilecache_cache']['config']['default'] = 0;
---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Page Caching',
+	'description' => 'Caching and optimisations',
+	'category' => 'fe',
+	'author' => 'Mike Street',
+	'author_company' => 'Liquid Light Ltd',
+	'author_email' => 'mike@liquidlight.co.uk',
+	'state' => 'stable',
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+			'staticfilecache' => '',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
+];
